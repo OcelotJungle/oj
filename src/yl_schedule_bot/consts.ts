647abb0d4b5c4154b75ae9e630ebfae47fa64fce
OcelotJungle <ocelotjungle@yandex.ru>
2022-02-09 00:58:00 +0500

    Create consts.ts

---
+++ src/yl_schedule_bot/consts.ts
@@ -0,0 +1,16 @@
+import { Role } from "./enums";
+
+export const DAYS_IN_WEEK = 7;
+export const EMPTY_COURIER_ROWS = 5;
+export const MAX_MORNING_START_TIME = 13;
+export const MIN_EVENING_END_TIME = 20;
+export const CAR_COURIER_ADDITION = "(авто)";
+export const WEEKEND_TEXT = "выходной";
+export const ADMIN = "ocelotjungle";
+export const ADMIN_TG = `@${ADMIN}`;
+export const MODULE_NAME = "yl_schedule_bot";
+export const ROLES: { [key in Role]: string } = {
+    admin: "админ",
+    courier: "курьер",
+    wa: "склад"
+}
