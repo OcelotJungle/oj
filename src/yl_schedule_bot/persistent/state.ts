bc9137fbd3e30d5e410da3627735f62e33bbc9cb
OcelotJungle <ocelotjungle@yandex.ru>
2022-02-12 00:56:42 +0500

    Fixed state loading

--- src/yl_schedule_bot/persistent/state.ts
+++ src/yl_schedule_bot/persistent/state.ts
@@ -5,31 +5,37 @@ import moment from "moment";
 export default class State implements IPersistent {
     path!: string;
 
-    isRestored!: boolean;
-    notificationMessageId?: number;
-    isFinalTableMade!: boolean;
+    isRestored: boolean = false;
+    notificationMessageId: number | null = null;
+    isFinalTableMade: boolean = false;
     week!: {
         start: moment.Moment;
-        end: moment.Moment
+        end: moment.Moment;
     }
 
-    constructor(path: string) { this.path = path }
+    constructor(path: string) {
+        this.path = path;
+        this.week = {
+            start: moment().startOf("week"),
+            end: moment().endOf("week")
+        };
+    }
 
     static getNewerState(a?: State, b?: State) {
-        if(a && b) return b.week.end.diff(a.week.start, "second") < 0 ? a : b;
+        if(a && b) return b.week.end.diff(a.week.start, "second") <= 0 ? a : b;
         else if(a || b) return (a ?? b)!;
         else throw new Error("Both compared states are invalid");
     }
 
     static from(raw: Partial<State>) {
-        const state = Object.create(State);
+        const state = Object.create(State.prototype) as State;
 
         state.isRestored = true;
-        state.notificationMessageId = raw?.notificationMessageId;
+        state.notificationMessageId = raw?.notificationMessageId ?? null;
         state.isFinalTableMade = raw?.isFinalTableMade ?? false;
         state.week = {
-            start: moment().startOf("week"),
-            end: moment().endOf("week")
+            start: moment(raw.week?.start),
+            end: moment(raw.week?.end)
         };
 
         return state;
@@ -44,13 +50,13 @@ export default class State implements IPersistent {
 
     async load() {
         try {
-            const state = JSON.parse((await fs.readFile(this.path)).toString()) as State;
+            const state = JSON.parse((await fs.readFile(this.path)).toString());
             this.update(State.getNewerState(this, State.from(state)));
         } catch(e) {}
     }
 
     async save() {
-        const { path, ...state } = this;
+        const { path, isRestored, ...state } = this;
         await fs.writeFile(this.path, JSON.stringify(state, null, 4));
     }
 
