de41cdff54a01b00ad921706e82385b31da278ab
OcelotJungle <ocelotjungle@yandex.ru>
2022-02-12 21:58:05 +0500

    Update moment.ts

--- src/utils/moment.ts
+++ src/utils/moment.ts
@@ -1,6 +1,6 @@
 import { Moment } from "moment";
 
 export function setMomentTime(moment: Moment, time: string) {
-    const [hours, minutes, seconds] = time.split(":").map(v => !Number.isNaN(+v) ? +v : undefined);
-    return moment.hours(hours ?? 0).minutes(minutes ?? 0).seconds(seconds ?? 0).milliseconds(0);
+    const [hours, minutes, seconds] = time.split(":").map(v => !Number.isNaN(+v) ? +v : 0);
+    return moment.hours(hours).minutes(minutes).seconds(seconds).milliseconds(0);
 }
